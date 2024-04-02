7201bfd6da74f27bed931ee7e1256b90a240b588
Evert Diaz <evertdiazb@gmail.com>
2024-04-02 10:07:09 -0500

    Creacion de cuentas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import * as algokit from '@algorandfoundation/algokit-utils';
 
 async function main() {
-    console.log('Hola Mundo!')
+    const algorand = algokit.AlgorandClient.defaultLocalNet();
+
+    // ===== Crear dos cuentas =====
+    const alice = algorand.account.random()
+    const bob = algorand.account.random();
+
+    console.log("Alice's Address:", alice.addr);
+
+    // ===== Mostrar información de la cuenta de Alice =====
+    console.log("Alice's Account:", await algorand.account.getInformation(alice.addr));
+
 }
 
 main();
