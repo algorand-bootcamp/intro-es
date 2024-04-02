c357737ff3eaf831af9bbcd08a6a1f746d18c95b
Evert Diaz <evertdiazb@gmail.com>
2024-04-02 10:07:47 -0500

    Creacion de ASA

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,27 @@ async function main() {
     // ===== Mostrar información de la cuenta de Alice =====
     console.log("Alice's Account:", await algorand.account.getInformation(alice.addr));
 
+    // ===== Enviar algunos ALGO a Alice =====
+    const dispenser = await algorand.account.dispenser();
+    await algorand.send.payment({
+        sender: dispenser.addr,
+        receiver: alice.addr,
+        amount: algokit.algos(10),
+    });
+
+    // ===== Ver el nuevo balance de Alice =====
+    console.log("Alice's Account", await algorand.account.getInformation(alice.addr));
+
+    // ===== Crear el ASA (Algorand Standard Asset) =====
+    const createResult = await algorand.send.assetCreate({
+        sender: alice.addr,
+        total: 100n,
+    });
+
+    // ===== Mostrar el AssetIndex del ASA creado =====
+    console.log("Create result confirmation",  createResult.confirmation);
+    const assetId = BigInt(createResult.confirmation.assetIndex!);
+
 }
 
 main();
